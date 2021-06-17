7caf6dd65f230ed3f8384c91a3cb390c7ac2a157
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-06-17 23:59:26 +1000

    added emoji search icon

--- src/playgrounds/EmojiSearch/components/EmojiSearchBar.jsx
+++ src/playgrounds/EmojiSearch/components/EmojiSearchBar.jsx
@@ -10,14 +10,14 @@ const EmojiSearchBar = (props) => {
                     onChange={props.changed}
                     type="text"
                     className="form-control"
-                    placeholder=""
+                    placeholder="Search emoji"
                     aria-label="Example text with button addon"
                     aria-describedby="button-addon1" />
                 <button
                     onClick={props.search}
                     className="btn btn-outline-secondary"
                     type="submit"
-                    id="button-addon1">Search</button>
+                    id="button-addon1">🔍</button>
                 {props.isLoaded ? null : <Spinner/>}
             </form>
         </div>
