bfb18a41cbf99f9312b173f3bc529b68d5152c38
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-06-18 21:47:08 +1000

    optimized performance for tic tac toe and calculator. Also added header tabs inorder to show notes of the project

--- src/components/PageContentContainer/PageContentContainer.jsx
+++ src/components/PageContentContainer/PageContentContainer.jsx
@@ -16,24 +16,33 @@ class PageContentContainer extends Component {
             playgrounds: [
                 {
                     name: "Emoji Search",
-                    desc: "Search emoji with words like Love or Smiling",
+                    desc: <p>Search emoji with words like Love or Smiling<br/>This emoji search tool uses API provided
+                        by emoji-api.com.<br/><a href="https://emoji-api.com/">Visit Emoji API website →</a></p>,
                     lastUpdate: "20/06/2021",
                     size: "col-xl-12 col-lg-12",
-                    playground: <EmojiSearch />
+                    playground: <EmojiSearch/>
                 },
                 {
                     name: "Calculator",
-                    desc: "Simple calculator like the one in iPhone",
+                    desc: <p>Simple calculator like the one in iPhone<br/>Auto text scaling method is reference from
+                        codepen calculator project. Link: <a
+                            href="https://codepen.io/mjijackson/pen/xOzyGX">codepen calculator →</a>
+                        <hr/>
+                        <b>Notes:</b><br/>18/06/2021 : Optimized calculator keys with React.memo() enclosed to prevent
+                        unnecessary
+                        re-rendering when each keys were pressed.</p>,
                     lastUpdate: "15/06/2021",
                     size: "col-xl-6 col-lg-6",
-                    playground: <Calculator />
+                    playground: <Calculator/>
                 },
                 {
                     name: "Tic Tac Toe",
-                    desc: "Tic Tac Toe game built by following React official tutorial",
+                    desc: <p>Tic Tac Toe game built by following React official tutorial. A great way to learn by doing.
+                        Link: <a href="https://reactjs.org/tutorial/tutorial.html">Reactjs Tic Tac Toe tutorial →</a>
+                    </p>,
                     lastUpdate: "10/06/2021",
                     size: "col-xl-6 col-lg-6",
-                    playground: <TicTacToe />
+                    playground: <TicTacToe/>
                 }
             ]
         }
--- src/components/PlaygroundContainer/PlaygroundContainer.jsx
+++ src/components/PlaygroundContainer/PlaygroundContainer.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const PlaygroundContainer = (props) => {
-    return (
-        <div className={props.size}>
-            <div className="card shadow mb-4">
-                <div
-                    className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 className="m-0 font-weight-bold text-primary">{props.name}</h6>
-                </div>
-                <div className="card-body">
-                    {props.desc}
-                    <br />
-                    <br />
-                    {props.children}
-                </div>
-                <div className="card-footer text-muted">
-                    - Last update : {props.lastUpdate}
+class PlaygroundContainer extends Component {
+    state = {
+        notesToggle: false
+    };
+
+    _notesToggleHandler = (e) => {
+        if (e.target.className.includes("active")) {
+            return;
+        }
+        const notesToggle = this.state.notesToggle;
+        this.setState({
+            notesToggle: !notesToggle
+        })
+    };
+
+    render() {
+        return (
+            <div className={this.props.size}>
+                <div className="card shadow mb-4">
+                    <div
+                        className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 className="m-0 font-weight-bold text-primary">{this.props.name}</h6>
+                        <ul className="nav nav-tabs card-header-tabs">
+                            <li className="nav-item">
+                                <button className={"nav-link " + (this.state.notesToggle ? "" : "active")}
+                                   onClick={this._notesToggleHandler}>{this.props.name}</button>
+                            </li>
+                            <li className="nav-item">
+                                <button className={"nav-link " + (this.state.notesToggle ? "active" : "")}
+                                   onClick={this._notesToggleHandler}>Notes</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="card-body">
+                        {this.state.notesToggle ? this.props.desc : this.props.children}
+                    </div>
+                    <div className="card-footer text-muted">
+                        - Last update : {this.props.lastUpdate}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default PlaygroundContainer;
