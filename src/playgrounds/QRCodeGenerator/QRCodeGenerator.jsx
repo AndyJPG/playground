bcedbb13c767b1b7433a24a1474d22fe9a0e640e
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-06-23 16:10:12 +1000

    integrated flow

---
+++ src/playgrounds/QRCodeGenerator/QRCodeGenerator.jsx
@@ -0,0 +1,15 @@
+// @flow
+
+import React, { Component } from 'react';
+
+class QRCodeGenerator extends Component {
+    render() {
+        return (
+            <div>
+                QR Code Generator
+            </div>
+        );
+    }
+}
+
+export default QRCodeGenerator;
