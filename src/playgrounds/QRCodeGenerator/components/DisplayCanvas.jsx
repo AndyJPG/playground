c797752870eefbb7f45602cbb03b379bef1b57a1
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-06-25 13:19:43 +1000

    finish implementing qrcode generator with flow syntax

--- src/playgrounds/QRCodeGenerator/components/DisplayCanvas.jsx
+++ src/playgrounds/QRCodeGenerator/components/DisplayCanvas.jsx
@@ -9,7 +9,7 @@ class displayCanvas extends React.PureComponent<Props> {
     render(): boolean | number | string | React$Element<*> | React$Portal | null {
         return (
             <div>
-                {this.props.qrCodeUrl ? <img src={this.props.qrCodeUrl} alt="QR code"/> : null}
+                {this.props.qrCodeUrl ? <img style={{maxWidth: "100%"}} src={this.props.qrCodeUrl} alt="QR code"/> : null}
             </div>
         );
     }
